eb596d1d0de9aa50dd8a4a64943829210c6f51a6
Efriedman1 <ericsean01@gmail.com>
2014-09-06 04:37:24 -0400

    rnh

--- MoleculeMash.spritebuilder/Source/MainScene.m
+++ MoleculeMash.spritebuilder/Source/MainScene.m
@@ -9,5 +9,37 @@
 #import "MainScene.h"
 
 @implementation MainScene
+{
+    CCNode *_pauseScene;
+    
+    CCNode *_mainPauseButton;
+    
+    BOOL paused;
+}
 
+-(void)pause{
+    if(self.paused) {
+    
+        self.paused = false;
+        self.userInteractionEnabled = true;
+        [[CCDirector sharedDirector] resume];
+        _mainPauseButton.visible = true;
+        [self removeChild:_pauseScene];
+
+    }
+    
+    else {
+        
+        [[CCDirector sharedDirector] pause];
+        _pauseScene = [CCBReader load:@"PauseNode" owner:self];
+        self.paused = true;
+        self.userInteractionEnabled = false;
+        _mainPauseButton.visible = false;
+        _pauseScene.positionType = (CCPositionTypeMake(CCPositionUnitNormalized, CCPositionUnitNormalized, CCPositionReferenceCornerTopLeft));
+        _pauseScene.position = ccp(0.5f,0.5f);
+        [self addChild:_pauseScene];
+        
+        
+    }
+}
 @end
